7c68091f7f7bb481723ee352386a902736e48ca2
Konrad Psiuk <konradpsiuk@gmail.com>
2024-07-09 22:16:54 +0100

    Refactor MQTT client codes and implement message receive callback This commit carries out a comprehensive refactoring of the MQTT client-related code. The MQTT configuration in the main module has been modified to make use of environment variables for broker details. Also, the default topic has been changed. Changes include newly added struct declaration for `PublishMessage` and `ReceivedMessage`, a redefinition of the MQTT configuration structure, and a new method that sets up the message callback function for handling MQTT messages received.
    A significant change is the implementation of a mechanism for interacting with GPIO pins based on MQTT messages received. This updated functionality has been integrated in the main module, including the initialization of a mutex variable for safety reasons in the section where MQTT messages are processed.
    
    The new updates also extend to the MQTT module where a new interface and related methods have been defined, including `SetMessageCallback` method in the MQTT client for setting up a callback function for incoming messages.
    
    The changes are meant to enhance functionality and ensure better, more error-resilient operation.

--- mqtt/mqtt_operator.go
+++ mqtt/mqtt_operator.go
@@ -7,7 +7,14 @@ import (
 	"time"
 )
 
-type Config struct {
+// MQTTConfig represents the configuration for an MQTT client. It contains the following fields:
+//
+// - Broker: The address of the MQTT broker to connect to.
+// - Topic: The topic to subscribe to or publish messages to.
+// - ClientID: The ID to use when connecting to the MQTT broker.
+// - Username: The username to use for authentication.
+// - Password: The password to use for authentication.
+type MQTTConfig struct {
 	Broker   string
 	Topic    string
 	ClientID string
@@ -15,58 +22,120 @@ type Config struct {
 	Password string
 }
 
-type MQTTClient struct {
-	client mqtt.Client
-	config Config
-	logger logger.Logger
+// PublishMessage Message to be published
+type PublishMessage struct {
+	Topic   string
+	Payload []byte
+	QoS     byte
 }
 
-func NewMQTTClient(config Config) *MQTTClient {
+// ReceivedMessage represents a message received from an MQTT broker.
+// It contains the topic and payload of the received message.
+type ReceivedMessage struct {
+	Topic   string
+	Payload []byte
+}
+
+// MQTTClient is a type that represents an MQTT client.
+// Note: MQTTClient requires the MQTTConfig and logger.LogOperator types to be properly initialized.
+type MQTTClient struct {
+	client         mqtt.Client
+	config         MQTTConfig
+	logger         logger.LogOperator
+	msgQueue       chan PublishMessage
+	messageHandler mqtt.MessageHandler
+}
+
+// NewMQTTClient initializes a new MQTTClient
+func NewMQTTClient(config MQTTConfig, logOperator logger.LogOperator) *MQTTClient {
 	opts := mqtt.NewClientOptions()
 	opts.AddBroker(config.Broker)
 	opts.SetClientID(config.ClientID)
 	opts.SetUsername(config.Username)
 	opts.SetPassword(config.Password)
-	opts.OnConnect = connectHandler
-	opts.OnConnectionLost = connectLostHandler
+	opts.KeepAlive = 10
+	opts.ConnectRetry = true
+	opts.AutoReconnect = true
+	opts.ConnectRetryInterval = 5 * time.Second
+
+	opts.OnConnectionLost = func(cl mqtt.Client, err error) {
+		logOperator.Error("Connection lost")
+	}
+	opts.OnConnect = func(mqtt.Client) {
+		logOperator.Info("Connection established")
+	}
+	opts.OnReconnecting = func(mqtt.Client, *mqtt.ClientOptions) {
+		logOperator.Info("Attempting to reconnect")
+	}
 
 	client := mqtt.NewClient(opts)
-	return &MQTTClient{client: client, config: config}
+	mc := &MQTTClient{
+		client:   client,
+		config:   config,
+		msgQueue: make(chan PublishMessage, 100),
+		logger:   logOperator,
+	}
+
+	go mc.connect()
+	go mc.startProcessing()
+
+	return mc
 }
 
-func (c *MQTTClient) Connect() error {
+// Connect attempts to connect the MQTT client to the broker
+func (c *MQTTClient) connect() {
+
+	c.logger.Info(fmt.Sprintf("Connecting to broker: %s", c.config.Broker))
+
 	if token := c.client.Connect(); token.Wait() && token.Error() != nil {
-		return fmt.Errorf("error connecting to broker: %v", token.Error())
+		c.logger.Error(fmt.Sprintf("Error connecting to broker: %v", token.Error()))
+		return
 	}
-	return nil
+
+	c.logger.Info("Connected to broker")
+
+	c.logger.Info(fmt.Sprintf("Subscribing to topic: %s", c.config.Topic))
+
+	// Subscribe to the topic upon successful connection
+	if token := c.client.Subscribe(c.config.Topic, 2, c.messageHandler); token.Wait() && token.Error() != nil {
+		c.logger.Error(fmt.Sprintf("Error subscribing to topic: %v", token.Error()))
+	}
+
+	c.logger.Info(fmt.Sprintf("Subscribed to topic: %s", c.config.Topic))
 }
 
-func (c *MQTTClient) Subscribe() error {
-	if token := c.client.Subscribe(c.config.Topic, 2, messageSubHandler); token.Wait() && token.Error() != nil {
-		return fmt.Errorf("error subscribing to topic: %v", token.Error())
-	}
-	return nil
+// Publish sends an MQTT message to the specified topic with the given payload and quality of service (QoS) level.
+// The message is added to the message queue and will be processed asynchronously.
+func (c *MQTTClient) Publish(topic string, payload []byte, qos byte) {
+	c.msgQueue <- PublishMessage{Topic: topic, Payload: payload, QoS: qos}
 }
 
-func (c *MQTTClient) Publish() {
-	go func() {
-		for {
-			text := "Hello MQTT QoS 2"
-			token := c.client.Publish(c.config.Topic, 2, false, text)
+// startProcessing processes messages from the queue and publishes them
+func (c *MQTTClient) startProcessing() {
+	c.logger.Info("Starting message processing")
+
+	for msg := range c.msgQueue {
+		if c.client.IsConnected() {
+			token := c.client.Publish(msg.Topic, msg.QoS, false, msg.Payload)
 			token.Wait()
-			time.Sleep(3 * time.Second)
+		} else {
+			c.logger.Error("Client not connected, could not publish message")
 		}
-	}()
+	}
 }
 
-var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
-	fmt.Println("Connected to MQTT broker")
-}
+// SetMessageCallback sets the callback function to handle incoming MQTT messages.
+// The callback function should have the signature func(message ReceivedMessage),
+// where ReceivedMessage is a struct containing the topic and payload of the received message.
+// When a message is received, it is converted to ReceivedMessage and passed to the callback function.
+func (c *MQTTClient) SetMessageCallback(callback func(message ReceivedMessage)) {
+	c.messageHandler = func(client mqtt.Client, msg mqtt.Message) {
+		// Convert the mqtt.Message to ReceivedMessage
+		receivedMsg := ReceivedMessage{
+			Topic:   msg.Topic(),
+			Payload: msg.Payload(),
+		}
 
-var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
-	fmt.Printf("Connection lost: %v\n", err)
-}
-
-var messageSubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
-	fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
+		callback(receivedMsg)
+	}
 }
