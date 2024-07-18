b802ffac6ef4f690d102530ab23d74cc518f36ce
Konrad Psiuk <konradpsiuk@gmail.com>
2024-07-18 19:37:58 +0100

    Refactor pin operator error handling and update dependencies
    Simplified error handling for pin operator operations by combining nested conditions. Updated dependencies in go.sum to include new modules needed for MQTT and WebSocket functionality.

--- main.go
+++ main.go
@@ -16,13 +16,12 @@ func main() {
 	var mutex sync.Mutex
 
 	logConfig := logger.LogConfig{
-		LogFilePath:   logFileName,
-		BufferSize:    1000,
-		FlushInterval: 1 * time.Second,
-		MaxSize:       1,
-		MaxBackups:    3,
-		MaxAge:        30,
-		LogLevel:      logger.InfoLevel,
+		LogFilePath:  logFileName,
+		BufferSize:   1000,
+		PollInterval: 1 * time.Second,
+		MaxSize:      1,
+		MaxBackups:   3,
+		MaxAge:       30,
 	}
 
 	log := logger.NewLogger(logConfig)
@@ -37,40 +36,41 @@ func main() {
 
 	log.Info("Starting Pin Operator")
 	pinOperator := initPinOperator(log)
-	pinOperatorOpenErr := pinOperator.Open()
+	err := pinOperator.Open()
 
-	if pinOperatorOpenErr != nil {
-		log.Error("Failed to open pin operator")
-	if err := pinOperator.Open(); err != nil {
-		log.Panic("Failed to open pin operator", err)
+	if err != nil {
+		log.Error("Failed to open pin operator", err)
+		if err := pinOperator.Open(); err != nil {
+			log.Panic("Failed to open pin operator", err)
+		}
+
+		defer func() {
+			err = pinOperator.Close()
+			if err != nil {
+				log.Error("Failed to close pin operator", err)
+			}
+		}()
+
+		pinOperator.SetOutputPin(10)
+
+		messageCallback := func(message mqtt.ReceivedMessage) {
+
+			mutex.Lock()
+			defer mutex.Unlock()
+			log.Info(fmt.Sprint("Received message: ", string(message.Payload)))
+
+			if string(message.Payload) == "SetHigh" {
+				pinOperator.SetHigh()
+			}
+			if string(message.Payload) == "SetLow" {
+				pinOperator.SetLow()
+			}
+		}
+
+		mqttClient := mqtt.NewMQTTClient(mqttConfig, log)
+
+		mqttClient.SetMessageCallback(messageCallback)
+
+		select {}
 	}
-
-	defer func() {
-		pinOperatorCloseError := pinOperator.Close()
-		if pinOperatorCloseError != nil {
-			log.Error("Failed to close pin operator")
-		}
-	}()
-
-	pinOperator.SetOutputPin(10)
-
-	messageCallback := func(message mqtt.ReceivedMessage) {
-
-		mutex.Lock()
-		defer mutex.Unlock()
-		log.Info(fmt.Sprint("Received message: ", string(message.Payload)))
-
-		if string(message.Payload) == "SetHigh" {
-			pinOperator.SetHigh()
-		}
-		if string(message.Payload) == "SetLow" {
-			pinOperator.SetLow()
-		}
-	}
-
-	mqttClient := mqtt.NewMQTTClient(mqttConfig, log)
-
-	mqttClient.SetMessageCallback(messageCallback)
-
-	select {}
 }
